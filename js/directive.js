868f6971287892b63727752a2422e98c59bdb2ff
Michael Knoch <info@michaelknoch.de>
2015-05-17 12:09:09 +0200

    refactor template

--- js/directive.js
+++ js/directive.js
@@ -1,4 +1,22 @@
-angular.module('locator.selection', []).directive('select ', function () {
+"use strict";
+
+angular.module('locator.selection', []).directive('listSelection ', function () {
+
+    var template = [
+        '<div ng-if="!multiple" class="relative" ng-class="{open: opened}"><a ng-click="trigger()">{{selectedModel.title}}</a>',
+        '<ul class="sub">',
+        '<li ng-click="select(value)" ng-repeat="value in values">{{value.title}}</li>',
+        '</ul>',
+        '</div>',
+
+        '<div ng-if="multiple" class="relative" ng-class="{open: opened}"><span class="fullwidth" ng-repeat="value in selectedModel">',
+        '<a ng-click="trigger()">{{value.title}}</a><span ng-if="twoPlus()" class="remove_item" ng-click="remove(value)">X</span></span>',
+        '<ul class="sub">',
+        '<li ng-click="select(item)" ng-repeat="item in values">{{item.title}}</li>',
+        '</ul>',
+        '</div>'
+    ];
+
     return {
         scope: {
             values: '=',
@@ -38,8 +56,7 @@ angular.module('locator.selection', []).directive('select ', function () {
 
                 $scope.trigger();
             };
-
-
+            
         },
         link: function ($scope) {
             $scope.$watch('selectedModel', function (newValue) {
@@ -53,7 +70,6 @@ angular.module('locator.selection', []).directive('select ', function () {
                 }
             }, true);
         },
-        template: '<div ng-if="!multiple" class="relative" ng-class="{open: opened}"><a ng-click="trigger()">{{selectedModel.title}}</a><ul class="sub"><li ng-click="select(value)" ng-repeat="value in values">{{value.title}}</li></ul></div>' +
-        '<div ng-if="multiple" class="relative" ng-class="{open: opened}"><span class="fullwidth" ng-repeat="value in selectedModel"><a ng-click="trigger()">{{value.title}}</a><span ng-if="twoPlus()" class="remove_item" ng-click="remove(value)">X</span></span><ul class="sub"><li ng-click="select(item)" ng-repeat="item in values">{{item.title}}</li></ul></div>'
+        template: template.join('')
     };
 });
