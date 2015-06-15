2445ae03cbe73b2f0e38e520fadd32fb1b962655
Timo Weiß <info@timo-weiss.com>
2015-06-15 15:12:28 +0200

    add additional values, handle open/close

--- js/directive.js
+++ js/directive.js
@@ -3,13 +3,13 @@
 angular.module('locator.selection', []).directive('listSelection', function () {
 
     var template = [
-        '<div ng-if="!multiple" class="relative selection" ng-class="{open: opened}"><a ng-click="trigger()"><img ng-if="icon" class="icon" ng-src="data:image/gif;base64,{{value.icon}}"> <span class="title">{{selectedModel.title}}</span></a>',
+        '<div ng-if="!multiple" active-popover click-value="selectedName" compare-to="mc.openElement" class="relative selection" ng-class="{open: selectedName === openElement}"><a ng-click="trigger()"><img ng-if="icon" class="icon" ng-src="data:image/gif;base64,{{value.icon}}"> <span class="title">{{selectedModel.title}}</span></a>',
         '<ul class="sub">',
         '<li ng-if="!selected(value)" ng-click="select(value)" ng-repeat="value in values">{{value.title}}</li>',
         '</ul>',
         '</div>',
 
-        '<div ng-if="multiple" class="relative selection" ng-class="{open: opened}"><span class="fullwidth" ng-repeat="value in selectedModel">',
+        '<div ng-if="multiple" class="relative selection" active-popover click-value="selectedName" compare-to="mc.openElement" ng-class="{open: selectedName === openElement}><span class="fullwidth" ng-repeat="value in selectedModel">',
         '<a ng-click="trigger()"><img ng-if="icon" class="icon" ng-src="data:image/gif;base64,{{value.icon}}"> <span class="title">{{value.title}}</span></a><span ng-if="twoPlus()" class="remove_item" ng-click="remove(value)">X</span></span>',
         '<ul class="sub">',
         '<li ng-if="!selected(item)" ng-click="select(item)" ng-repeat="item in values">{{item.title}}</li>',
@@ -19,10 +19,12 @@ angular.module('locator.selection', []).directive('listSelection', function () {
 
     return {
         scope: {
-            values: '=',
+            'values': '=',
             'selectedModel': '=',
             'multiple': '=',
-            'icon': '='
+            'icon': '=',
+            'selectedName': '=',
+            'openElement': '='
         },
 
         controller: function ($scope, hotkeys, $timeout, $rootScope, lodash) {
