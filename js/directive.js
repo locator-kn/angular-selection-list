36f2420d8167ac02f3bbe144ea51b4dba3c00912
Michael Knoch <info@michaelknoch.de>
2015-07-01 16:57:09 +0200

    remove relative

--- js/directive.js
+++ js/directive.js
@@ -3,7 +3,7 @@
 angular.module('locator.selection', []).directive('listSelection', function () {
 
     var template = [
-        '<div ng-if="!multiple" active-popover click-value="selectedName" compare-to="mc.openElement" class="relative selection" ng-class="{open: selectedName === openElement}"><a ng-click="trigger()"><img ng-if="icon" class="icon" ng-src="data:image/gif;base64,{{value.icon}}"> <span class="title">{{selectedModel.title}}</span></a>',
+        '<div ng-if="!multiple" active-popover click-value="selectedName" compare-to="mc.openElement" class="selection" ng-class="{open: selectedName === openElement}"><a ng-click="trigger()"><img ng-if="icon" class="icon" ng-src="data:image/gif;base64,{{value.icon}}"> <span class="title">{{selectedModel.title}}</span></a>',
         '<ul class="sub">',
         '<li ng-if="!selected(value)" ng-click="select(value)" ng-repeat="value in values">{{value.title}}</li>',
         '</ul>',
